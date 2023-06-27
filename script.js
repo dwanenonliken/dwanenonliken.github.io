f8fe8ea75c7b19a951c620e00bc93823f9e9abd3
dwanenonliken <131609833+dwanenonliken@users.noreply.github.com>
2023-06-27 14:45:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Get form values
+  var name = document.getElementById("name").value;
+ 
